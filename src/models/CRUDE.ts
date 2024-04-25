36b1756591ae47f52edbe15ae2a4f75b3c4ade56
ThiagoAndo <ando.thiago@gmail.com>
2024-04-25 18:58:56 +0100

    Ading CRUD actions

---
+++ src/models/CRUDE.ts
@@ -0,0 +1,21 @@
+import User from "../models/user";
+import Request from "../models/request";
+import Comment from "../models/comments";
+import Repli from "../models/replies";
+
+export interface Read {
+  read: "many" | "one";
+  field: {id:number}|{username:string}|{key:null};
+  index: number;
+}
+
+
+export type mongoRet =
+  | User
+  | Request
+  | Comment
+  | Repli
+  | User[]
+  | Request[]
+  | Comment[]
+  | Repli[];
