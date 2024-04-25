0b5147558149b5dc10b30d2f6cec33d7e01037d0
ThiagoAndo <ando.thiago@gmail.com>
2024-04-25 13:41:53 +0100

    users working

--- src/routes/request.routes.ts
+++ src/routes/request.routes.ts
@@ -4,4 +4,16 @@ import { collections } from "../services/database.service";
 
 export const requestRouter = express.Router();
 
-// 
+requestRouter.use(express.json());
+
+// requestRouter.get("/", async (_req: Request, res: Response) => {
+//   try {
+//     // Call find with an empty filter object, meaning it returns all documents in the collection. Saves as Game array to take advantage of types
+//     const users = await collections.user?.find({}).toArray();
+//     console.log(users);
+
+//     res.status(200).send(users);
+//   } catch (error: any) {
+//     res.status(500).send(error.message);
+//   }
+// });
