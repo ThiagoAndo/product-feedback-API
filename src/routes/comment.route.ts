9a2fb7484c93de13b2777dd160a610c0e473d461
ThiagoAndo <ando.thiago@gmail.com>
2024-04-25 16:27:08 +0100

    Comment route working

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from "express";
-import { ObjectId } from "mongodb";
+// import { ObjectId } from "mongodb";
 import { collections } from "../services/database.service";
 
-export const userRouter = express.Router();
+export const commentRouter = express.Router();
 
-userRouter.use(express.json());
+commentRouter.use(express.json());
 
-userRouter.get("/", async (_req: Request, res: Response) => {
+commentRouter.get("/", async (_req: Request, res: Response) => {
   try {
     // Call find with an empty filter object, meaning it returns all documents in the collection. Saves as Game array to take advantage of types
-    const users = await collections[0].find({}).toArray();
+    const users = await collections[2].find({}).toArray();
     res.status(200).send(users);
   } catch (error: any) {
     res.status(500).send(error.message);
